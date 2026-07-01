cc23aab454260da32c2174a6a3f3e45685442cde
ssvenkatesu <ssivasrivenkatesu31@gmail.com>
2026-07-01 11:59:15 +0530

    Problem No 1 Completed

---
+++ Product/Main.java
@@ -0,0 +1,23 @@
+package Product;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String productId = sc.nextLine();
+        String productName = sc.nextLine();
+        double price = sc.nextDouble();
+        int stockQuantity = sc.nextInt();
+        double discount = sc.nextDouble();
+
+        Product p = new Product(productId, productName, price, stockQuantity);
+
+        p.applyDiscount(discount);
+
+        System.out.println(p.getPrice());
+        System.out.println(p.getStockQuantity());
+
+        sc.close();
+    }
+}
---
+++ Product/Product.java
@@ -0,0 +1,33 @@
+package Product;
+
+public class Product {
+    private String productId;
+    private String productName;
+    private double price;
+    private int stockQuantity;
+
+    public Product(String productId, String productName, double price, int stockQuantity) {
+        this.productId = productId;
+        this.productName = productName;
+        this.price = price;
+        this.stockQuantity = stockQuantity;
+    }
+
+    public void applyDiscount(double discount) {
+        if (discount < 0) {
+            return;
+        }
+        this.price -= this.price * discount / 100.0;
+        if (this.price < 0) {
+            this.price = 0;
+        }
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getStockQuantity() {
+        return stockQuantity;
+    }
+}
